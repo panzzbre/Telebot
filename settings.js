25b63bafb67d732aa1fd106d93a9e8db44b98873
panzzbre <karminmin192@gmail.com>
2024-07-11 15:30:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "7216764587:AAHhxqg4CgYtdd4GChAkfXuYCX9_EjS69V4" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "Lanzz - Botz" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ PanzzOwn Tele ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6283124327057" //your telegram number
+global.OWNER = ["https://t.me/PanzzStore123", "https://t.me/PanzzStore123"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
